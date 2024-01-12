a7b316871cfec56434b58e16abfd2d8b8dee7574
do-sg <do.seongyeong1@gmail.com>
2024-01-12 18:06:29 +0900

    feat: 관리자 학부모 리스트 페이지 퍼블리싱 진행중

--- src/pages/adminPage/GuardianList.js
+++ src/pages/adminPage/GuardianList.js
@@ -1,4 +1,16 @@
+import { Select } from "antd";
+import Search from "antd/es/input/Search";
 import React, { lazy } from "react";
+import {
+  HeaderLeft,
+  HeaderRight,
+  HeaderWrap,
+  ListId,
+  ListItem,
+  ListName,
+  StyledSearch,
+  StyledSelect,
+} from "../../styles/guardianlist";
 
 // 원아관리 상세
 // const LazyStudentDetails = lazy(() =>
@@ -15,9 +27,64 @@ import React, { lazy } from "react";
 // const LazyStudentNewDetails = lazy(() =>
 //   import("./pages/adminPage/StudentNewDetails"),
 // );
-
+const handleChange = value => {
+  console.log(value);
+};
+const onSearch = (value, info) => console.log(info?.source, value);
 const GuardianList = () => {
-  return <h1>학부모 관리 리스트</h1>;
+  return (
+    <>
+      <HeaderWrap>
+        <HeaderLeft>학부모 관리</HeaderLeft>
+        <HeaderRight>
+          <StyledSelect
+            labelInValue
+            defaultValue={{
+              value: "반 선택",
+              label: "반 선택",
+            }}
+            onChange={handleChange}
+            options={[
+              {
+                value: "1",
+                label: "무궁화반",
+              },
+              {
+                value: "2",
+                label: "해바라기반",
+              },
+              {
+                value: "3",
+                label: "장미반",
+              },
+              {
+                value: "-1",
+                label: "졸업",
+              },
+              {
+                value: "-2",
+                label: "퇴소",
+              },
+            ]}
+          />
+          <StyledSearch placeholder="이름을 입력하세요." onSearch={onSearch} />
+
+          <button>선택회원삭제</button>
+        </HeaderRight>
+      </HeaderWrap>
+
+      <div className="GuardianListWrap">
+        <ListItem>
+          <input type="checkbox" />
+          <ListId>bong11</ListId>
+          <ListName>봉미선</ListName>
+          <div className="ListChild">해바라기반 신짱구</div>
+          <em className="ListPhoneNumber">010.0000.0000</em>
+          <button>정보수정</button>
+        </ListItem>
+      </div>
+    </>
+  );
 };
 
 export default GuardianList;

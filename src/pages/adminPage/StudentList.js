b48d62b0a3371e1083a0a396bc3b9b1b4e159cf2
do-sg <do.seongyeong1@gmail.com>
2024-01-15 18:33:21 +0900

    feat: 학부모 관리 페이지 퍼블리싱 진행중

--- src/pages/adminPage/StudentList.js
+++ src/pages/adminPage/StudentList.js
@@ -1,10 +1,77 @@
 import React from "react";
 import ContentLayout from "../../layouts/ContentLayout";
+import { Select } from "antd";
+import Search from "antd/es/input/Search";
 
+const handleChange = value => {
+  console.log(value);
+};
 const StudentList = () => {
   return (
     <ContentLayout>
-      <h1>원아관리 리스트</h1>
+      <div className="StudentTop">
+        <div className="StudentTopLeft">원생 관리</div>
+        <div className="StudentTopRight">
+          <Select
+            labelInValue
+            defaultValue={{
+              value: "",
+              label: "반 선택",
+            }}
+            style={{
+              width: 100,
+            }}
+            onChange={handleChange}
+            options={[
+              {
+                value: "1",
+                label: "무궁화반",
+              },
+              {
+                value: "2",
+                label: "해바라기반",
+              },
+              {
+                value: "3",
+                label: "장미반",
+              },
+              {
+                value: "-1",
+                label: "졸업",
+              },
+              {
+                value: "-2",
+                label: "퇴소",
+              },
+            ]}
+          />
+          <Search
+            placeholder="검색어를 입력하세요."
+            style={{
+              width: 200,
+            }}
+            allowClear
+          />
+        </div>
+        <button>선택 반 변경</button>
+        <button>선택졸업</button>
+        <button>선택취소</button>
+      </div>
+      <div className="StudentMain">
+        <ul className="StudentListWrap">
+          <li className="StudentListItem">
+            <div className="StudentImg">
+              <img></img>
+            </div>
+            <div className="StudentInfo">
+              <img></img>
+              <p>해바라기반</p>
+              <img></img>
+              <span>신짱구</span>
+            </div>
+          </li>
+        </ul>
+      </div>
     </ContentLayout>
   );
 };

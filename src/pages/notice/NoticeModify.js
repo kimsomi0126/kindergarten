c98ff4b9c7c34e6974bad8ea02b90025da167b03
dsa9925 <dev.dsa9925@gmail.com>
2024-01-18 19:37:14 +0900

    feat : 유치원 상세페이지 퍼블리싱 및 등록 페이지 진행중

--- src/pages/notice/NoticeModify.js
+++ src/pages/notice/NoticeModify.js
@@ -1,6 +1,50 @@
 import React from "react";
 import ContentLayout from "../../layouts/common/ContentLayout";
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Upload } from "antd";
 
+const fileList = [
+  {
+    uid: "0",
+    name: "xxx.png",
+    status: "uploading",
+    percent: 33,
+  },
+  {
+    uid: "-1",
+    name: "yyy.png",
+    status: "done",
+    url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    thumbUrl:
+      "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+  },
+  {
+    uid: "-2",
+    name: "zzz.png",
+    status: "error",
+  },
+];
+const App = () => (
+  <>
+    <Upload
+      action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+      listType="picture"
+      defaultFileList={[...fileList]}
+    >
+      <Button icon={<UploadOutlined />}>Upload</Button>
+    </Upload>
+    <br />
+    <br />
+    <Upload
+      action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+      listType="picture"
+      defaultFileList={[...fileList]}
+      className="upload-list-inline"
+    >
+      <Button icon={<UploadOutlined />}>Upload</Button>
+    </Upload>
+  </>
+);
 const NoticeModify = () => {
   return (
     <ContentLayout>

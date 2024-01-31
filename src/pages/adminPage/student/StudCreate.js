ca39e5f48577c8ff029b6b4677229892e78d943c
do-sg <do.seongyeong1@gmail.com>
2024-01-31 20:30:53 +0900

    feat : 원생등록 api 연동중

--- src/pages/adminPage/student/StudCreate.js
+++ src/pages/adminPage/student/StudCreate.js
@@ -43,18 +43,19 @@ const props = {
   headers: {
     authorization: "authorization-text",
   },
-  onChange(info) {
-    if (info.file.status !== "uploading") {
-      console.log(info.file, info.fileList);
-    }
-    if (info.file.status === "done") {
-      message.success(`${info.file.name} 파일 첨부 성공`);
-    } else if (info.file.status === "error") {
-      message.error(`${info.file.name} 파일 첨부 실패`);
-    }
-  },
+  // onChange(info) {
+  //   if (info.file.status !== "uploading") {
+  //     console.log(info.file, info.fileList);
+  //   }
+  //   if (info.file.status === "done") {
+  //     message.success(`${info.file.name} 파일 첨부 성공`);
+  //   } else if (info.file.status === "error") {
+  //     message.error(`${info.file.name} 파일 첨부 실패`);
+  //   }
+  // },
 };
 
+// 초기값
 const initState = {
   pic: "",
   dto: {
@@ -70,47 +71,75 @@ const initState = {
 };
 
 const StudentCreate = () => {
-  // 등록
+  // 원생 등록
   const [student, setStudent] = useState(initState);
-  // 정보 업데이트
-  const handleChange = e => {
-    student[e.target.name] = e.target.value;
-    setStudent({ ...student });
-  };
-  const uploadRef = useRef(null);
-  // 파일 업로드 실행
-  const handleClick = () => {
-    // const files = uploadRef.current.files;
-    const files = uploadRef.current.fileList;
-    // console.log(uploadRef.current.fileList[0]);
-    const filesTotal = files.length;
-    const formData = new FormData();
-    for (let i = 0; i < filesTotal; i++) {
-      formData.append("files", files[i]);
-    }
-    formData.append("pic", files[0].name);
-    formData.append("kidNm", student.kidNm);
-    formData.append("iclass", student.iclass);
-    formData.append("gender", student.gender);
-    formData.append("birth", student.birth);
-    formData.append("address", student.address);
-    formData.append("memo", student.memo);
-    formData.append("emerNm", student.emerNm);
-    formData.append("emerNb", student.emerNb);
-    console.log(student);
-    // 제품 정보 전송하기
-    // postStudentCreate({ student: formData, successFn, failFn, errorFn });
+  const [fileList, setFileList] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const handleChange = info => {
+    let fileList = [...info.fileList].filter(file => !!file.status);
+    setFileList(fileList);
   };
 
-  const successFn = result => {
-    console.log(result);
+  const onFinish = async data => {
+    console.log("fileList", fileList);
+    const formData = new FormData();
+    console.log("data", data);
+    const dto = new Blob(
+      [
+        JSON.stringify({
+          kidNm: data.kidNm,
+          iclass: data.iclass,
+          gender: data.gender,
+          birth: data.birth,
+          address: data.address,
+          memo: data.memo,
+          emerNm: data.emerNm,
+          emerNb: data.emerNb,
+        }),
+      ],
+      { type: "application/json" },
+    );
+
+    formData.append("dto", dto);
+
+    fileList.forEach(file => {
+      formData.append("pics", file.originFileObj);
+    });
+
+    // formData를 서버에 전송
+    postStudentCreate({
+      successFn: handleSuccess,
+      failFn: handleFailure,
+      errorFn: handleError,
+      studenet: formData,
+    });
   };
-  const failFn = result => {
-    console.log(result);
+  const handleSuccess = response => {
+    setIsModalVisible(true);
+    // 성공적으로 업로드 완료 후 처리할 작업을 추가할 수 있습니다.
   };
-  const errorFn = result => {
-    console.log(result);
+
+  const handleFailure = errorMessage => {
+    // 업로드 실패 시 처리할 작업을 추가할 수 있습니다.
+    // Modal.error({
+    //   title: "앨범 업로드 실패",
+    //   content: errorMessage,
+    // });
   };
+
+  const handleError = error => {
+    console.error("앨범 업로드 오류:", error);
+    // Modal.error({
+    //   title: "앨범 업로드 중 오류 발생",
+    //   content:
+    //     "서버 오류 또는 네트워크 문제가 발생했습니다. 다시 시도해주세요.",
+    // });
+  };
+
+  // 파일 업로드 실행
+  const handleClick = () => {};
+
   // 모달창 적용
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [isCancelModalOpen, setIsCancelModalOpen] = useState(false);
@@ -182,38 +211,120 @@ const StudentCreate = () => {
         <PageTitle>원생 등록</PageTitle>
       </StudFormTop>
       <StudFormWrap>
-        {/* 기본정보 */}
-        <BasicInfo>
-          <p>기본 정보</p>
-          <BasicInfoForm>
-            <BasicInfoItem>
-              <Form.Item
-                name="kidNm"
-                onChange={e => handleChange(e)}
-                style={{
-                  width: "33%",
-                }}
-              >
-                <Input placeholder="이름" value={student.kidNm} />
-              </Form.Item>
-              <Form.Item
-                name="birth"
-                onChange={e => handleChange(e)}
-                style={{
-                  width: "33%",
-                }}
-              >
-                <DatePicker
+        <Form onFinish={onFinish}>
+          {/* 기본정보 */}
+          <BasicInfo>
+            <p>기본 정보</p>
+            <BasicInfoForm>
+              <BasicInfoItem>
+                <Form.Item
+                  name="kidNm"
                   style={{
-                    width: "100%",
+                    width: "33%",
                   }}
-                  placeholder="생년월일"
-                  value={student.birth}
-                />
+                >
+                  <Input placeholder="이름" />
+                </Form.Item>
+                <Form.Item
+                  name="birth"
+                  style={{
+                    width: "33%",
+                  }}
+                >
+                  <DatePicker
+                    style={{
+                      width: "100%",
+                    }}
+                    placeholder="생년월일"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="gender"
+                  style={{
+                    width: "33%",
+                  }}
+                >
+                  <Select
+                    labelInValue
+                    defaultValue={{
+                      value: "",
+                      label: (
+                        <span style={{ color: " rgba(0, 0, 0, 0.25) " }}>
+                          성별 선택
+                        </span>
+                      ),
+                    }}
+                  >
+                    <Select.Option value="1">남자</Select.Option>
+                    <Select.Option value="0">여자</Select.Option>
+                  </Select>
+                </Form.Item>
+              </BasicInfoItem>
+              <Form.Item name="address">
+                <BasicInfoCode>
+                  <Form.Item>
+                    <Input
+                      disabled
+                      type="text"
+                      value={zodecode}
+                      onChange={e => setZonecode(e.target.value)}
+                      placeholder="우편 번호"
+                    />
+                  </Form.Item>
+                  <button onClick={toggleHandler}>주소 검색</button>
+                  {isOpen && (
+                    <Modal
+                      title="주소 검색"
+                      open={isModalOpen}
+                      onOk={handleOk}
+                      onCancel={handleCancel}
+                      footer={null}
+                    >
+                      <DaumPostcode
+                        theme={themeObj}
+                        style={postCodeStyle}
+                        onComplete={completeHandler}
+                        onClose={closeHandler}
+                      />
+                    </Modal>
+                  )}
+                </BasicInfoCode>
+                <BasicInfoAdress>
+                  <Form.Item
+                    style={{
+                      width: "50%",
+                    }}
+                  >
+                    <Input
+                      disabled
+                      type="text"
+                      placeholder="주소 입력"
+                      value={address}
+                      onChange={e => setAddress(e.target.value)}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    style={{
+                      width: "50%",
+                    }}
+                  >
+                    <Input
+                      type="text"
+                      placeholder="상세 주소 입력"
+                      value={detailedAddress}
+                      onChange={inputChangeHandler}
+                    />
+                  </Form.Item>
+                </BasicInfoAdress>
               </Form.Item>
+            </BasicInfoForm>
+          </BasicInfo>
+          {/* 재원정보 */}
+          <ClassInfo>
+            <p>재원 정보</p>
+            <ClassInfoForm>
               <Form.Item
-                name="gender"
-                onChange={e => handleChange(e)}
+                name="iclass"
                 style={{
                   width: "33%",
                 }}
@@ -224,203 +335,85 @@ const StudentCreate = () => {
                     value: "",
                     label: (
                       <span style={{ color: " rgba(0, 0, 0, 0.25) " }}>
-                        성별 선택
+                        반 선택
                       </span>
                     ),
                   }}
                 >
-                  <Select.Option value="1">남자</Select.Option>
-                  <Select.Option value="0">여자</Select.Option>
+                  <Select.Option value="1">무궁화반</Select.Option>
+                  <Select.Option value="2">해바라기반</Select.Option>
+                  <Select.Option value="3">장미반</Select.Option>
                 </Select>
               </Form.Item>
-            </BasicInfoItem>
-            <BasicInfoCode>
-              <Form.Item name="adress" onChange={e => handleChange(e)}>
-                <Input
-                  disabled
-                  type="text"
-                  value={zodecode}
-                  onChange={e => setZonecode(e.target.value)}
-                  placeholder="우편 번호"
-                />
+            </ClassInfoForm>
+          </ClassInfo>
+          {/* 프로필 이미지 */}
+          <ImgInfo>
+            <p>프로필 이미지</p>
+            <ImgInfoForm>
+              <Form.Item>
+                <Upload {...props}>
+                  <Button icon={<UploadOutlined />}>파일 첨부</Button>
+                </Upload>
               </Form.Item>
-              <button onClick={toggleHandler}>주소 검색</button>
-              {isOpen && (
-                <Modal
-                  title="주소 검색"
-                  open={isModalOpen}
-                  onOk={handleOk}
-                  onCancel={handleCancel}
-                  footer={null}
+            </ImgInfoForm>
+          </ImgInfo>
+          {/* 비상연락처 */}
+          <PhoneInfo>
+            <p>비상 연락처</p>
+            <PhoneInfoForm>
+              <div
+                style={{
+                  display: "flex",
+                  gap: "1rem",
+                }}
+              >
+                <Form.Item
+                  name="emerNm"
+                  style={{
+                    width: "50%",
+                  }}
                 >
-                  <DaumPostcode
-                    theme={themeObj}
-                    style={postCodeStyle}
-                    onComplete={completeHandler}
-                    onClose={closeHandler}
+                  <Input type="text" placeholder="이름 입력" />
+                </Form.Item>
+                <Form.Item
+                  name="emerNb"
+                  rules={[
+                    {
+                      pattern: /^\d{10,11}$/,
+                      message: "휴대폰 번호를 올바르게 입력하세요.",
+                    },
+                  ]}
+                  style={{
+                    width: "50%",
+                  }}
+                >
+                  <Input
+                    type="tel"
+                    placeholder="휴대폰 번호 입력 // 하이픈(-) 제외"
                   />
-                </Modal>
-              )}
-            </BasicInfoCode>
-            <BasicInfoAdress>
-              <Form.Item
-                style={{
-                  width: "50%",
-                }}
-              >
-                <Input
-                  disabled
-                  type="text"
-                  placeholder="주소 입력"
-                  value={address}
-                  onChange={e => setAddress(e.target.value)}
+                </Form.Item>
+              </div>
+            </PhoneInfoForm>
+          </PhoneInfo>
+          {/* 관리자메모 */}
+          <AdminMemo>
+            <p>관리자 메모</p>
+            <AdminMemoForm>
+              <Form.Item name="memo">
+                <TextArea
+                  value={value}
+                  onChange={e => setValue(e.target.value)}
+                  placeholder="관리자 메모"
+                  autoSize={{
+                    minRows: 4,
+                    maxRows: 5,
+                  }}
                 />
               </Form.Item>
-              <Form.Item
-                style={{
-                  width: "50%",
-                }}
-              >
-                <Input
-                  type="text"
-                  placeholder="상세 주소 입력"
-                  value={detailedAddress}
-                  onChange={inputChangeHandler}
-                />
-              </Form.Item>
-            </BasicInfoAdress>
-          </BasicInfoForm>
-        </BasicInfo>
-        {/* 재원정보 */}
-        <ClassInfo>
-          <p>재원 정보</p>
-          <ClassInfoForm>
-            {/* <Form.Item
-              style={{
-                width: "33%",
-              }}
-            >
-              <DatePicker
-                placeholder="입학 날짜"
-                style={{
-                  width: "100%",
-                }}
-              />
-            </Form.Item> */}
-            {/* <Form.Item
-              style={{
-                width: "33%",
-              }}
-            >
-              <Select
-                labelInValue
-                defaultValue={{
-                  value: "",
-                  label: (
-                    <span style={{ color: " rgba(0, 0, 0, 0.25) " }}>
-                      재원 상태
-                    </span>
-                  ),
-                }}
-              >
-                <Select.Option value="0">재원중</Select.Option>
-                <Select.Option value="-1">졸업</Select.Option>
-                <Select.Option value="-2">퇴소</Select.Option>
-              </Select>
-            </Form.Item> */}
-            <Form.Item
-              name="iclass"
-              onChange={e => handleChange(e)}
-              style={{
-                width: "33%",
-              }}
-            >
-              <Select
-                labelInValue
-                defaultValue={{
-                  value: "",
-                  label: (
-                    <span style={{ color: " rgba(0, 0, 0, 0.25) " }}>
-                      반 선택
-                    </span>
-                  ),
-                }}
-              >
-                <Select.Option value="1">무궁화반</Select.Option>
-                <Select.Option value="2">해바라기반</Select.Option>
-                <Select.Option value="3">장미반</Select.Option>
-              </Select>
-            </Form.Item>
-          </ClassInfoForm>
-        </ClassInfo>
-        {/* 프로필 이미지 */}
-        <ImgInfo>
-          <p>프로필 이미지</p>
-          <ImgInfoForm>
-            <Form.Item>
-              <Upload ref={uploadRef} {...props}>
-                <Button icon={<UploadOutlined />}>파일 첨부</Button>
-              </Upload>
-            </Form.Item>
-          </ImgInfoForm>
-        </ImgInfo>
-        {/* 비상연락처 */}
-        <PhoneInfo>
-          <p>비상 연락처</p>
-          <PhoneInfoForm>
-            <Form
-              style={{
-                display: "flex",
-                gap: "1rem",
-              }}
-            >
-              <Form.Item
-                style={{
-                  width: "50%",
-                }}
-              >
-                <Input
-                  type="text"
-                  placeholder="이름 입력"
-                  value={student.emerNm}
-                />
-              </Form.Item>
-              <Form.Item
-                name="tel"
-                rules={[
-                  {
-                    pattern: /^\d{10,11}$/,
-                    message: "휴대폰 번호를 올바르게 입력하세요.",
-                  },
-                ]}
-                style={{
-                  width: "50%",
-                }}
-              >
-                <Input
-                  type="tel"
-                  placeholder="휴대폰 번호 입력 // 하이픈(-) 제외"
-                  value={student.emerNb}
-                />
-              </Form.Item>
-            </Form>
-          </PhoneInfoForm>
-        </PhoneInfo>
-        {/* 관리자메모 */}
-        <AdminMemo value={student.memo}>
-          <p>관리자 메모</p>
-          <AdminMemoForm>
-            <TextArea
-              value={value}
-              onChange={e => setValue(e.target.value)}
-              placeholder="관리자 메모"
-              autoSize={{
-                minRows: 4,
-                maxRows: 5,
-              }}
-            />
-          </AdminMemoForm>
-        </AdminMemo>
+            </AdminMemoForm>
+          </AdminMemo>
+        </Form>
       </StudFormWrap>
       <BottomBt>
         <GreenBtn onClick={handleClick}>등록</GreenBtn>
